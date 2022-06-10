b2ed4365dca8c1e858778d2bbdd2e560ab785e27
Lewis <lewis.murgor@student.moringaschool.com>
2022-06-10 15:09:27 +0300

    send welcome email

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from . import auth
 from ..models import User
 from .forms import LoginForm,RegistrationForm
 from .. import db
+from ..email import mail_message
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -27,6 +28,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
+        mail_message("Welcome to Wins","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
     title = "New Account"
     return render_template('auth/register.html',registration_form = form,title = title)
