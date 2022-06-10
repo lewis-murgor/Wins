9fc6c6502295a3443919b41f95d03a2edecc60d9
Lewis <lewis.murgor@student.moringaschool.com>
2022-06-10 10:54:53 +0300

    create profile page

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,1 +1,8 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,RadioField,SelectField,ValidationError
+from wtforms.validators import InputRequired,Email
+
+class UpdateProfile(FlaskForm):
+    
+    bio = TextAreaField('Tell us about you.',validators = [InputRequired()])
+    submit = SubmitField('Submit')
--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lewis:lewis29@localhost/wins'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 
 class ProdConfig(Config):
